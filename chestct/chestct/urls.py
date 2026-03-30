a9a3ed9706ac7bdd1eeac1a48f38550dfaf5a909
Varshita Krishna <selenophile.solitude@gmail.com>
2026-03-30 09:45:12 +0530

    Add files via upload

---
+++ chestct/chestct/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect
+from django.conf.urls.static import static
+from django.conf import settings
+def home_redirect(request):
+    return redirect('login')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home_redirect),
+    path('', include('diagnosis.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
